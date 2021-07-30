a82696432789fd617f15c98c6de9f8477ca652de
keyboard-king <utkarshbansal01@gmail.com>
2021-07-30 18:18:43 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PollCreateComponent } from './poll-create/poll-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollCreateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/poll-create/poll-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poll-create',
+  templateUrl: './poll-create.component.html',
+  styleUrls: ['./poll-create.component.scss']
+})
+export class PollCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
