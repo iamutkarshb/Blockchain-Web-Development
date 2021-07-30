be1fbc051eb546b85d317b89bd8c2ab258f6ec65
keyboard-king <utkarshbansal01@gmail.com>
2021-07-30 21:29:37 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PollComponent } from './poll/poll.component';
+import { PollVoteComponent } from './poll-vote/poll-vote.component';
+import { PollService } from './poll-service/poll.service';
+import { Web3Service } from './blockchain/web3.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PollCreateComponent
+    PollCreateComponent,
+    PollComponent,
+    PollVoteComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  providers: [PollService, Web3Service],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
