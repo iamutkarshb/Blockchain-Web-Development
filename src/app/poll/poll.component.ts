8e947fa423c97a12bc1544cb23dac23cd094fe05
keyboard-king <utkarshbansal01@gmail.com>
2021-07-30 21:34:01 +0530

    update

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-poll',
   templateUrl: './poll.component.html',
-  styleUrls: ['./poll.component.scss']
+  styleUrls: ['./poll.component.scss'],
 })
 export class PollComponent implements OnInit {
+  @Input() question: string;
+  @Input() votes: number[]; // [0, 1, 5, 7]
+  @Input() voted: boolean;
+  @Input() pollImage: string;
 
-  constructor() { }
+  numberOfVotes: number;
+
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.votes.length) {
+      this.numberOfVotes = this.votes.reduce((acc, curr) => {
+        return (acc += curr);
+      });
+    }
   }
-
 }
